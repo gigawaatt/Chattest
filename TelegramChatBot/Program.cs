669112ce795da8cf52905d4801874cd091ac8025
gigawaatt <57654659+gigawaatt@users.noreply.github.com>
2021-06-03 09:11:01 +0300

    1

--- TelegramChatBot/Program.cs
+++ TelegramChatBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Telegram.Bot;
 
 namespace TelegramChatBot
 {
@@ -7,6 +8,16 @@ namespace TelegramChatBot
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string token = "870171424:AAG8yCjqyc0fimQN7P6-iOth4gNhsoc6tBs";
+            var bot = new TelegramBotClient(token);
+            bot.OnMessage += Bot_OnMessage;
+            bot.StartReceiving();
+            Console.ReadKey();
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+
         }
     }
 }
---
+++ TelegramChatBot/Quiz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramChatBot
+{
+    public class Quiz
+    {
+        public Quiz(string path = "data.txt")
+        {
+            var lines = File.ReadAllLines(path);
+            var questions = lines.
+                Select(line => line.Split('|')).
+                Select(line => (line[0], line[1])).
+                ToList();
+
+            var random = new Random();
+            var count = questions.Count;
+
+            var score = 0;
+
+        }
+    }
+}
